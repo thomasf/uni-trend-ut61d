90128bf728b436ee39a69154cdfa5620c46e3b64
Thomas Frössman <thomasf@jossystem.se>
2014-08-17 22:01:45 +0200

    Somewhat better startup script

--- startdmm.sh
+++ startdmm.sh
@@ -1,3 +1,11 @@
-sudo ./he2325u/suspend.HE2325U.sh
-./he2325u/he2325u | ./dmmut61bcd/dmmut61bcd.pl
+#!/bin/bash
 
+if [ ! $UID == 0 ]; then
+  echo "use sudo"
+  exit 1
+fi
+
+# TODO suspend thing does not work but the script works anyway
+./he2325u/suspend.HE2325U.sh
+
+./he2325u/he2325u | ./dmmut61bcd/dmmut61bcd.pl "$@"
